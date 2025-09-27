fda0e80c4837b14c4468649f8c7a6cc369d16178
Hemant Kumar <hemant861983@gmail.com>
2025-09-27 18:14:56 +0530

    change

--- client/src/Component/Footer/footer.js
+++ client/src/Component/Footer/footer.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
 
-function footer() {
+function Footer() {
   return (
-    <div>
-    
-    </div>
-  )
+    <footer className="footer">
+      <div className="footer-container">
+        
+        {/* Left with Logo */}
+        <div className="footer-left">
+          <img src="/navbar_img/K91PNGLOGO.png" alt="" className="footer-logo" />
+          <p>Bringing your vision to life with stunning visuals and creativity.</p>
+        </div>
+
+        {/* Center Links */}
+        <div className="footer-links">
+          <h3>Quick Links</h3>
+          <a href="#about">About Us</a>
+          <a href="#services">Services</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </div>
+
+        {/* Right Socials */}
+        <div className="footer-socials">
+          <h3>Follow Us</h3>
+          <div className="social-icons">
+            <a href="#">Facebook</a>
+            <a href="#">Instagram</a>
+            <a href="#">Twitter</a>
+            <a href="#">LinkedIn</a>
+          </div>
+        </div>
+      </div>
+
+      {/* Bottom line */}
+      <div className="footer-bottom">
+        © {new Date().getFullYear()} K91 Productions. All Rights Reserved.
+      </div>
+    </footer>
+  );
 }
 
-export default footer
+export default Footer;
